b7d2915b065d23739264488b6a4dd370dd2ebd4f
Nitr0 <bidintha2006@gmail.com>
2025-01-09 12:07:13 +0530

    Chat Interface Added

--- Appv1/src/main.jsx
+++ Appv1/src/main.jsx
@@ -1,10 +1,30 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import './index.css'
+// import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import ChatInterface from './ChatInterface';  // Import the ChatInterface component
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// Render routes with Router
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />         {/* Main page with mic buttons */}
+        <Route path="/chat" element={<ChatInterface />} />  {/* Chat page */}
+      </Routes>
+    </Router>
+  </React.StrictMode>
+);
